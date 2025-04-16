65e8f0b664ec68bd10a474d5ee69a502395e818b
hoonihoon12345 <soundtruckapp@gmail.com>
2025-04-16 16:02:30 +0900

    chore: 설정 파일 추가

---
+++ build-logic/convention/src/main/kotlin/com/mockapi/KotlinAndroid.kt
@@ -0,0 +1,52 @@
+package com.mockapi
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+/**
+ * Configure base Kotlin with Android options
+ */
+internal fun Project.configureKotlinAndroid(
+  commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+  commonExtension.apply {
+    compileSdk = 35
+
+    defaultConfig {
+      minSdk = 21
+    }
+
+    compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_17
+      targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    lint {
+      abortOnError = false
+    }
+
+    kotlinOptions {
+      // Treat all Kotlin warnings as errors (disabled by default)
+      allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
+      freeCompilerArgs = freeCompilerArgs + listOf(
+        "-Xcontext-receivers",
+        "-opt-in=kotlin.RequiresOptIn",
+        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        "-opt-in=com.google.accompanist.pager.ExperimentalPagerApi",
+        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+        "-opt-in=androidx.lifecycle.compose.ExperimentalLifecycleComposeApi",
+        "-opt-in=androidx.compose.animation.ExperimentalSharedTransitionApi",
+      )
+
+      // Set JVM target
+      jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+  }
+}
+
+fun CommonExtension<*, *, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+  (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
